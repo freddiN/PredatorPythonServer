ddd7a1a1bad5ce601d724e89c05f4f5c2c10e624
Freddi <muff99@outlook.com>
2016-08-07 01:21:19 +0200

    Format

--- funcs_file.py
+++ funcs_file.py
@@ -24,18 +24,18 @@ def watchFolder():
 # alles in eine Liste schreiben
 # wenn START und ENDE da sind: alles an einen handler geben	
 def parseFile(filename):
-	appendLog('parseFile von ' + filename);
-	if not filename.endswith('.action'):
-		appendLog('parseFile ignored ' + filename + ' : wrong filetype');
-		return;
-    
+  appendLog('parseFile von ' + filename);
+  if not filename.endswith('.action'):
+    appendLog('parseFile ignored ' + filename + ' : wrong filetype');
+    return;
+
   listAction = [];
   with open(filename, 'r') as fp:
     for line in fp:
-			if len(line) > 0 and not line.startswith('#'):   #leerzeilen und kommentare
+      if len(line) > 0 and not line.startswith('#'):
         appendLog('line:>' + line.strip() + '<');
-				listAction.append(line.strip());
-       
+        listAction.append(line.strip());
+
   if len(listAction) > 0 and listAction.pop(0) == "START" and listAction.pop() == "END":
     appendLog("Start and END found");
     deleteFile(filename);
